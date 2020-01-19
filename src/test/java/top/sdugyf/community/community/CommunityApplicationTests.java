9819ea86ccd564db966ab405f3fb0a787105accb
SDU-GYF <a956895479@gmail.com>
2020-01-19 18:05:03 +0800

    init repo

---
+++ src/test/java/top/sdugyf/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package top.sdugyf.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
