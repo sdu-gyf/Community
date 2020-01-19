a36cf1154de7ca09cb5acd6ec650698f4a5dcd0e
SDU-GYF <a956895479@gmail.com>
2020-01-19 18:48:42 +0800

    add index and bootstrap

--- src/main/java/top/sdugyf/community/community/controller/HelloController.java
+++ src/main/java/top/sdugyf/community/community/controller/HelloController.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name", name);
-        return "hello";
-    }
+    @GetMapping("/")
+    public String index() { return "index" ;};
 }
+
